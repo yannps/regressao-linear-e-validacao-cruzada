84ac9be2552797f84480e788679aa43780f68cfb
yannps <yann.pereira@dcx.ufpb.br>
2020-02-13 23:09:38 -0300

    .

--- trabalho1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-dados = pd.read_csv('items.csv')
-
-itens = dados[["asin", "brand", "title" , "url", "image", "rating", "reviewUrl", "totalReviews", "prices"]]
-
-print(itens)
